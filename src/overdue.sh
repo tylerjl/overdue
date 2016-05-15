2f237778c3f6b65ce80e48235d8e790aefdc46b0
James Barnett <jbarnet4@tulane.edu>
2016-05-15 18:11:57 -0500

    comment file

--- src/overdue.sh
+++ src/overdue.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# read in file list from pacman hook
+# must add / since files passed by the hook don't have it
 files=""
 while read -r f; do
     files="${files} /${f}"
 done
 
+# get the PID's of processes using the files
+# echo is needed to remove leading whitespace
 pids=$(echo $(fuser $files 2> /dev/null))
+
+# get and sort the systemd unit names, removing duplicates
 services="$(ps -o unit= $pids)"
 services=$(echo "$services" | sort | uniq)
 
