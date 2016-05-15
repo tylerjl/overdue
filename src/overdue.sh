d7f818b06c3ae1a9913caed6737ab05d359a9d53
James Barnett <jbarnet4@tulane.edu>
2016-05-15 18:24:24 -0500

    check if files is empty; if so, exit. use same style throughout script

--- src/overdue.sh
+++ src/overdue.sh
@@ -6,26 +6,22 @@ files=""
 while read -r f; do
     files="${files} /${f}"
 done
+[[ -z "$files" ]] && exit 0
 
 # get the PID's of processes using the files
 # echo is needed to remove leading whitespace
-pids=$(echo $(fuser $files 2> /dev/null))
-
-# get and sort the systemd unit names, removing duplicates
 # have to check if we actually got any PID's; otherwise ps returns
 # user1000.service, etc.
+pids=$(echo $(fuser $files 2> /dev/null))
+[[ -z "$pids" ]] && exit 0
 
-if [[ -n "$pids" ]]; then
+# get and sort the systemd unit names, removing duplicates
+services="$(ps -o unit= $pids)"
+services=$(echo "$services" | sort | uniq)
+[[ -z "$services" ]] && exit 0
 
-    services="$(ps -o unit= $pids)"
-    services=$(echo "$services" | sort | uniq)
-
-    if [[ -n "$services" ]]; then
-        echo "The following systemd services have stale file handles open to"
-        echo "libraries that have been upgraded:"
-        for i in $services; do
-            echo -e "  $i"
-        done
-    fi
-
-fi
+echo "The following systemd services have stale file handles open to"
+echo "libraries that have been upgraded:"
+for i in $services; do
+    echo -e "  $i"
+done
