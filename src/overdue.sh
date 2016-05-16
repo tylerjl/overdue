0e1a7188a1cb478bb3b7a8718c76e9ef7478b001
James Barnett <jbarnet4@tulane.edu>
2016-05-16 09:29:36 -0500

    add back depends and use bullet point

--- src/overdue.sh
+++ src/overdue.sh
@@ -11,8 +11,10 @@ pids="$(lsof -d DEL | awk 'NR>1 {printf $2" "}')"
 services="$(ps -o unit= $pids | sort -u)"
 [[ -z "$services" ]] && exit 0
 
-echo "The following daemons/units have stale file handles open to
-libraries that have been upgraded. Consider restarting them
-if they should reference updated shared libraries.
+echo "The following daemons/units have stale file handles open to"
+echo "libraries that have been upgraded. Consider restarting them"
+echo "if they should reference updated shared libraries."
 
-$services"
+for i in $services; do
+    echo "● $i"
+done
