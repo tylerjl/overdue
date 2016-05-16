c61e6406359b9131c919c4293df2afd1aed76a80
James Barnett <jbarnet4@tulane.edu>
2016-05-15 22:05:00 -0500

    syntax

--- src/overdue.sh
+++ src/overdue.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
 
-if [ "$EUID" != "0" ] ; then
+if [[ "$EUID" != "0" ]] ; then
     echo 'You must be root to run this program.'
     exit 1
 fi
 
-pids=$(lsof -d DEL | awk 'NR>1 {printf $2" "}')
+pids="$(lsof -d DEL | awk 'NR>1 {printf $2" "}')"
 [[ -z "$pids" ]] && exit 0
 
 services="$(ps -o unit= $pids | sort -u)"
-if [ ! -z "$services" ] ; then
-    echo "The following daemons/units have stale file handles open to
+[[ -z "$services" ]] && exit 0
+
+echo "The following daemons/units have stale file handles open to
 libraries that have been upgraded. Consider restarting them
 if they should reference updated shared libraries.
 
 $services"
-fi
