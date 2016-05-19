6f68841a00dfb8fefa00b0fbf7db9ea1f0823284
James Barnett <jbarnet4@tulane.edu>
2016-05-18 20:50:32 -0500

    filter lsof output only for files starting with /usr/lib

--- src/overdue.sh
+++ src/overdue.sh
@@ -5,7 +5,7 @@ if [[ "$EUID" != "0" ]] ; then
     exit 1
 fi
 
-pids="$(lsof -d DEL | awk 'NR>1 {printf $2" "}')"
+pids="$(lsof -d DEL | awk '$8~/\/usr\/lib/ {printf $2" "}')"
 [[ -z "$pids" ]] && exit 0
 
 services="$(ps -o unit= $pids | sort -u)"
